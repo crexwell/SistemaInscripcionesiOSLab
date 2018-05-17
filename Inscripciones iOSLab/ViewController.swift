cf6d1c9f309f35bd46c32fb57f2a50766d700fc2
L Daniel De San Pedro <daniel@m22.mx>
2018-05-17 16:00:30 -0500

    First commit

---
+++ Inscripciones iOSLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inscripciones iOSLab
+//
+//  Created by L Daniel De San Pedro on 17/05/18.
+//  Copyright © 2018 iOS Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
